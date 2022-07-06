d665fc68928219e8ba743b0021917132388544b5
astral4 <88992929+astral4@users.noreply.github.com>
2022-07-06 15:46:59 -0400

    add penguin-stats drop matrix

---
+++ calc.py
@@ -0,0 +1,10 @@
+from constants import *
+import requests
+import pandas as pd
+
+drop_matrix = requests.get("https://penguin-stats.io/PenguinStats/api/v2/result/matrix").json()
+
+drop_data = (
+    pd.json_normalize(drop_matrix, ["matrix"])
+      .drop(["stdDev", "start", "end"], axis=1)
+)
