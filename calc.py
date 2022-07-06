adfd347b719a8f9a60b64ed74eee2ef01c5d8ff5
astral4 <88992929+astral4@users.noreply.github.com>
2022-07-06 15:47:46 -0400

    filter out stages with not enough runs

--- calc.py
+++ calc.py
@@ -7,4 +7,5 @@ drop_matrix = requests.get("https://penguin-stats.io/PenguinStats/api/v2/result/
 drop_data = (
     pd.json_normalize(drop_matrix, ["matrix"])
       .drop(["stdDev", "start", "end"], axis=1)
+      .pipe(lambda df: df[df["times"] >= MIN_RUN_THRESHOLD])
 )
