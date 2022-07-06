0c983395932c71c759259c80cc2f23a143ec5fc7
astral4 <88992929+astral4@users.noreply.github.com>
2022-07-06 18:51:11 -0400

    analyze permanent stages only

--- calc.py
+++ calc.py
@@ -1,10 +1,14 @@
 from constants import *
 import requests
 import pandas as pd
+import numpy as np
 
 def calc_drop_rates(df):
     df["drop_rate"] = df["quantity"] / df["times"]
     return df
+
+def exclude_limited_stages(df):
+    return np.logical_or(df.str.startswith("main"), df.str.endswith("perm"))
 
 drop_matrix = requests.get("https://penguin-stats.io/PenguinStats/api/v2/result/matrix")\
                       .json()
@@ -13,6 +17,7 @@ drop_data = (
     pd.json_normalize(drop_matrix, ["matrix"])
       .drop(["stdDev", "start", "end"], axis=1)
       .pipe(lambda df: df[df["times"] >= MIN_RUN_THRESHOLD])
+      .pipe(lambda df: df[exclude_limited_stages(df["stageId"])])
       .pipe(lambda df: df[~df["itemId"].isin(EXCLUDED_ITEMS)])
       .pipe(calc_drop_rates)
       .drop(["times", "quantity"], axis=1)
