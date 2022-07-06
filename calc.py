d3e30eab9ed78e193f7db6cdfbad2d88c409390d
astral4 <88992929+astral4@users.noreply.github.com>
2022-07-06 17:16:51 -0400

    calculate item drop rate

--- calc.py
+++ calc.py
@@ -2,6 +2,10 @@ from constants import *
 import requests
 import pandas as pd
 
+def calc_drop_rates(df):
+    df["drop_rate"] = df["quantity"] / df["times"]
+    return df
+
 drop_matrix = requests.get("https://penguin-stats.io/PenguinStats/api/v2/result/matrix")\
                       .json()
 
@@ -10,4 +14,6 @@ drop_data = (
       .drop(["stdDev", "start", "end"], axis=1)
       .pipe(lambda df: df[df["times"] >= MIN_RUN_THRESHOLD])
       .pipe(lambda df: df[~df["itemId"].isin(EXCLUDED_ITEMS)])
+      .pipe(calc_drop_rates)
+      .drop(["times", "quantity"], axis=1)
 )
