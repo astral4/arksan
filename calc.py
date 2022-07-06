2ddaa2b575db42e0b9ceb50ef9e7e4f0f0fc3c70
astral4 <88992929+astral4@users.noreply.github.com>
2022-07-06 17:12:12 -0400

    filter out excluded items

--- calc.py
+++ calc.py
@@ -2,10 +2,12 @@ from constants import *
 import requests
 import pandas as pd
 
-drop_matrix = requests.get("https://penguin-stats.io/PenguinStats/api/v2/result/matrix").json()
+drop_matrix = requests.get("https://penguin-stats.io/PenguinStats/api/v2/result/matrix")\
+                      .json()
 
 drop_data = (
     pd.json_normalize(drop_matrix, ["matrix"])
       .drop(["stdDev", "start", "end"], axis=1)
       .pipe(lambda df: df[df["times"] >= MIN_RUN_THRESHOLD])
+      .pipe(lambda df: df[~df["itemId"].isin(EXCLUDED_ITEMS)])
 )
--- constants.py
+++ constants.py
@@ -1,1 +1,8 @@
 MIN_RUN_THRESHOLD = 100
+EXCLUDED_ITEMS = ("randomMaterial_1", "randomMaterial_2", "randomMaterial_3", "randomMaterial_4", "randomMaterial_5", "randomMaterial_6",
+                  "ap_supply_lt_010",
+                  "furni",
+                  "3211", "3221", "3231", "3241", "3251", "3261", "3271", "3281", # chips
+                  "3212", "3222", "3232", "3242", "3252", "3262", "3272", "3282", # chip packs
+                  "3112", "3113", "3114", # carbon stick, carbon brick, carbon pack
+                  )
