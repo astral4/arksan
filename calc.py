256f621ba5a2e0794370f3ccb327e41468d2f8ce
astral4 <88992929+astral4@users.noreply.github.com>
2022-07-06 23:27:09 -0400

    fix stage ids

--- calc.py
+++ calc.py
@@ -9,11 +9,24 @@ def calc_drop_rates(df):
 def exclude_limited_stages(stage_ids):
     return stage_ids.str.startswith("main") | stage_ids.str.endswith("perm")
 
-drop_matrix = requests.get("https://penguin-stats.io/PenguinStats/api/v2/result/matrix")\
-                      .json()
+def fix_stage_ids(df):
+    df.index = df.index.str.removesuffix("_perm")
+    return df
+
+drop_matrix = (
+    requests.get("https://penguin-stats.io/PenguinStats/api/v2/result/matrix")
+            .json()
+            ["matrix"]
+)
+
+stage_data = (
+    requests.get("https://raw.githubusercontent.com/Kengxxiao/ArknightsGameData/master/zh_CN/gamedata/excel/stage_table.json")
+            .json()
+            ["stages"]
+)
 
 drop_data = (
-    pd.json_normalize(drop_matrix, record_path=["matrix"])
+    pd.json_normalize(drop_matrix)
       .drop(["stdDev", "start", "end"], axis=1)
       .pipe(lambda df: df[df["times"] >= MIN_RUN_THRESHOLD])
       .pipe(lambda df: df[exclude_limited_stages(df["stageId"])])
@@ -22,4 +35,5 @@ drop_data = (
       .drop(["times", "quantity"], axis=1)
       .pivot(index="stageId", columns="itemId", values="drop_rate")
       .fillna(0)
+      .pipe(fix_stage_ids)
 )
