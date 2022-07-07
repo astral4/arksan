bdb06a856c175977fffe39a0d6a503dc337b5132
astral4 <88992929+astral4@users.noreply.github.com>
2022-07-06 20:34:10 -0400

    prepare LP solution: inequality constraint matrix

--- calc.py
+++ calc.py
@@ -14,11 +14,13 @@ drop_matrix = requests.get("https://penguin-stats.io/PenguinStats/api/v2/result/
                       .json()
 
 drop_data = (
-    pd.json_normalize(drop_matrix, ["matrix"])
+    pd.json_normalize(drop_matrix, record_path=["matrix"])
       .drop(["stdDev", "start", "end"], axis=1)
       .pipe(lambda df: df[df["times"] >= MIN_RUN_THRESHOLD])
       .pipe(lambda df: df[exclude_limited_stages(df["stageId"])])
       .pipe(lambda df: df[~df["itemId"].isin(EXCLUDED_ITEMS)])
       .pipe(calc_drop_rates)
       .drop(["times", "quantity"], axis=1)
+      .pivot(index="stageId", columns="itemId", values="drop_rate")
+      .fillna(0)
 )
