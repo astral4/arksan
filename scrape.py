16ce477c4d556ab0573db5966c50ff9625ab9b65
astral4 <88992929+astral4@users.noreply.github.com>
2022-07-15 19:21:01 -0400

    reformat for clarity

--- scrape.py
+++ scrape.py
@@ -2,6 +2,8 @@ from constants import *
 import pandas as pd
 import dateparser
 
-df = pd.read_html(OPERATOR_URL, converters={"国服上线时间": dateparser.parse})[0]
-
-print(df)
+char_debut_dates = (
+    pd.read_html(OPERATOR_URL,
+                 converters={"国服上线时间": dateparser.parse})
+      [0]
+)
