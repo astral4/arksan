76e2d91899ad00df1f7006b77e4208be55561f94
astral4 <88992929+astral4@users.noreply.github.com>
2022-07-15 19:09:21 -0400

    make datetimes pandas-readable

--- scrape.py
+++ scrape.py
@@ -1,4 +1,7 @@
 from constants import *
 import pandas as pd
+import dateparser
 
-df = pd.read_html(OPERATOR_URL)[0]
+df = pd.read_html(OPERATOR_URL, converters={"国服上线时间": dateparser.parse})[0]
+
+print(df)
