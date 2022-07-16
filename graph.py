b2cfd48e04d4c7fbfb176657176687e5892c2129
astral4 <88992929+astral4@users.noreply.github.com>
2022-07-16 01:46:37 -0400

    merge char_debut_times and char_upgrade_costs

--- graph.py
+++ graph.py
@@ -7,8 +7,6 @@ char_debut_times = (
     pd.read_html(CHAR_DEBUT_TIMES_URL,
                  converters={"国服上线时间": dateparser.parse})
       [0]
-      .set_index("干员")
-      .query("稀有度 == 6")
       .drop(columns=["稀有度", "国服上线途径", "主要获得方式", "干员预告"])
 )
 
@@ -28,6 +26,10 @@ char_upgrade_costs = (
              columns="id",
              values="count")
       .reset_index()
-      .set_index("name")
-      .reindex(index=char_debut_times.index)
+      .merge(char_debut_times, how="left", left_on="name", right_on="干员")
+      .drop(columns=["name", "干员"])
+      .set_index(["appellation", "国服上线时间"])
+      .sort_index(level="国服上线时间")
 )
+
+print(char_upgrade_costs)
