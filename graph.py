323ef3e3ab7b7ef1a21006f8b2e009e664193e13
astral4 <88992929+astral4@users.noreply.github.com>
2022-07-15 20:24:07 -0400

    never mind, we're separating scrape.py and calc.py

---
+++ graph.py
@@ -0,0 +1,12 @@
+from constants import *
+import pandas as pd
+import dateparser
+
+char_debut_dates = (
+    pd.read_html(OPERATOR_URL,
+                 converters={"国服上线时间": dateparser.parse})
+      [0]
+      .set_index("干员")
+      .query("稀有度 == 6")
+      .drop(columns=["稀有度", "国服上线途径", "主要获得方式", "干员预告"])
+)
