b8550af8096c39912aabbd18e694f0fc85a38ccb
astral4 <88992929+astral4@users.noreply.github.com>
2022-07-16 15:00:31 -0400

    (8/8) calculate MSVs based on char debut times

--- graph.py
+++ graph.py
@@ -3,7 +3,7 @@ import pandas as pd
 import dateparser
 import requests
 from collections import defaultdict
-from calc import get_sanity_values
+import calc
 
 def adjust_time(df):
     df["国服上线时间"] += pd.Timedelta(hours=16)
@@ -45,9 +45,16 @@ char_upgrade_costs = (
 
 sanity_costs = defaultdict(float)
 
-for char_data, upgrade_cost in char_upgrade_costs.head().iterrows():
+for char_data, upgrade_cost in char_upgrade_costs.iterrows():
     char_name, debut_time = char_data
     
-    sanity_costs[char_name] = upgrade_cost.to_numpy(na_value=0).dot(get_sanity_values(debut_time))
+    if debut_time < pd.to_datetime("2019-12-24 08:00:00"): # ch6
+        upgrade_cost = upgrade_cost.drop(labels=["31013", "31014", "31023", "31024"])
+    if debut_time < pd.to_datetime("2020-11-01 08:00:00"): # ch8
+        upgrade_cost = upgrade_cost.drop(labels=["31033", "31034", "30145"])
+    if debut_time < pd.to_datetime("2021-09-17 08:00:00"): # ch9
+        upgrade_cost = upgrade_cost.drop(labels=["31043", "31044", "31053", "31054"])
+
+    sanity_costs[char_name] = upgrade_cost.to_numpy(na_value=0).dot(calc.get_sanity_values(debut_time, upgrade_cost.index.to_numpy()))
 
 print(sanity_costs)
